1627bc7882cfd4769af4c6b2207b5fe733005762
AnaLuizaDias <99154345+AnaLuizaDias@users.noreply.github.com>
2022-04-21 13:08:29 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+import Cabecalho from "./components/Cabecalho.vue";
+import Rodape from "./components/rodape.vue";
+import BlocoPrincp from "./components/BlocoPrincp.vue";
+
+export default {
+  components: { MenuLateral, Cabecalho, Rodape, BlocoPrincp },
+};
+</script>
+<template>
+  <MenuLateral />
+  <Cabecalho />
+  <BlocoPrincp />
+  <Rodape />
+</template>
+<style></style>
---
+++ src/components/BlocoPrincp.vue
@@ -0,0 +1,16 @@
+<script>
+import Bloco from "@/components/Bloco.vue";
+export default {
+  components: { Bloco },
+};
+</script>
+<template>
+  <main id="content" >
+    <Bloco class="Bloco" />
+    <Bloco class="Bloco" />
+    <Bloco class="Bloco" />
+    <Bloco class="Bloco" />
+    <Bloco class="Bloco" />
+    <Bloco class="Bloco" />
+  </main>
+</template>
